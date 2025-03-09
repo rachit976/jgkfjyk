ff7f12c9d074c1f0e3c934291e97ef8995c1e2f3
rachit976 <mukeshsoni38@gmail.com>
2025-03-09 20:04:07 +0530

    Add files via upload

---
+++ #INPUT THE VALUE OF TERMS.py
@@ -0,0 +1,11 @@
+#INPUT THE VALUE OF TERMS
+n = int(input("enter the value of terms"))
+
+sum = 0 #inialise
+i = 1 #inialise
+while i<=n: #loop will run from 1 to  n 
+    sum=sum+i
+    i=i+1
+
+
+    print("\nsum = ",sum)
